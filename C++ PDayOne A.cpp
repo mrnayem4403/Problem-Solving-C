528be48341243a10303dbd8d67db3a4699afb922
Mostafizur Rahman Nayem <mrnayem4403@gmail.com>
2022-07-05 15:32:22 +0600

    C++ PDay one A

---
+++ C++ PDayOne A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    int a,b,h;
+
+    cin>>a;
+    cin>>b;
+    cin>>h;
+
+    int area = (a+b)*h/2;
+    cout<<area;
+
+
+    return 0;
+}
