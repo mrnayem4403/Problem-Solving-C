71e32eaac1c8e5238f83a5b9043757e9eeb97d81
Mostafizur Rahman Nayem <mrnayem4403@gmail.com>
2022-07-05 15:45:58 +0600

    Pday1

---
+++ C++  PDay1 B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+
+{
+
+    char v;
+    cin>>v;
+    if(v == 'a' || v == 'e' || v == 'o' || v == 'i' || v == 'u')
+    {
+          cout<<"vowel"<<endl;
+    }
+    else
+    {
+        cout<< "consonant" <<endl;
+    }
+
+    return 0;
+}
